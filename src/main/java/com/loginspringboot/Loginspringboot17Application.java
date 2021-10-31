61137356553f9cec131a17e56cb099d0bbe34766
TuDoan2704 <doantund2812@gmail.com>
2021-10-31 18:47:42 +0700

    add

---
+++ src/main/java/com/loginspringboot/Loginspringboot17Application.java
@@ -0,0 +1,13 @@
+package com.loginspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Loginspringboot17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Loginspringboot17Application.class, args);
+	}
+
+}
